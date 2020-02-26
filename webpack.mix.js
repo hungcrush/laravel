011a4da577efad14e671992e10b169531c173b75
HUNG.TV <hung.tv@hanbiro.com>
2020-02-26 16:18:02 +0700

    Update webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,59 @@
-const mix = require('laravel-mix');
-
+let glob = require('glob');
+let mix  = require('laravel-mix');
 /*
  |--------------------------------------------------------------------------
- | Mix Asset Management
+ | Hanbiro Asset Management
  |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+const getFileExtension = (fileName) => {
+    let ext = fileName.split('.');
+    ext = ext[ext.length - 1];
+
+    if(ext === 'scss') ext = 'sass';
+    if(ext === 'js' || ext === 'jsx') ext = 'react';
+    return ext;
+}
+
+const mixingModule = (module) => {
+    if(module.sources) {
+        sources.map(mixData => {
+            let method = getFileExtension(mixData[0]);
+            mix[method](resourcePath + mixData[0], publicPath + mixData[1]);
+        })
+    }
+}
+
+const resourcePath = 'vendor/hanbiro/';
+const publicPath = 'public/assets/';
+
+const externals = {
+    'jquery': {
+        'commonjs': 'jquery',
+        'commonjs2': 'jquery',
+        'amd': 'jquery',
+        'root': '$'
+    },
+    'lodash': '_',
+    "window": "window",
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'react-is': 'ReactIs',
+    'prop-types': 'PropTypes',
+    'moment': 'moment',
+    'classnames': 'classNames'
+};
+
+mix.webpackConfig({
+    externals
+})
+    .options({ extractVueStyles: true });
+
+let Base = require('./vendor/hanbiro/base/webpack.config.js');
+mixingModule(Base);
+
+// scan packages project
+glob.sync('./packages/*/webpack.config.js').forEach(config => {
+    let ModuleConfig = require(config);
+    mixingModule(ModuleConfig);
+});
