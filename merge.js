271254a670346cb39150745a7b25fe447751c9d0
Harry Tr <hung.tv@hanbiro.com>
2020-02-27 19:32:21 +0700

    Update merge package

--- merge.js
+++ merge.js
@@ -1,10 +1,20 @@
 const fs = require('fs')
-
+const glob = require('glob')
 const sharedPackageJson = require('./package.json');
 const BasePackage = require('./vendor/hanbiro/base/package.json');
 
-Object.assign(BasePackage.dependencies, sharedPackageJson.dependencies);
-Object.assign(BasePackage.devDependencies, sharedPackageJson.devDependencies);
-Object.assign(BasePackage.scripts, sharedPackageJson.scripts);
+const mergePackages = (module) => {
+    Object.assign(BasePackage.dependencies, module.dependencies);
+    Object.assign(BasePackage.devDependencies, module.devDependencies);
+    Object.assign(BasePackage.scripts, module.scripts);
+}
+
+mergePackages(sharedPackageJson);
+
+//-- scan packages project
+glob.sync('./packages/*/package.json').forEach(config => {
+    let module = require(config);
+    mergePackages(module);
+});
 
 fs.writeFileSync('./package.json', JSON.stringify(BasePackage, null, 2))
